9bca9dc2646b567d799cfd40ccd37d35c4bc9809
Stian Soiland-Reyes <s.soilandreyes@uva.nl>
2020-03-03 12:17:15 +0000

    zipfiles samples as generated from current seed/snakemake

--- data/zenodo-records/zipfiles.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xzcat zenodo-records-json-2019-09-16-filtered.jsonseq.xz |  jq -r '. | select(.metadata.access_right == "open") | .metadata.resource_type.type as $rectype | . as $rec | ( .files[]?  | select(.type == "zip") ) | [$rec.id, $rec.links.self, $rec.links.doi, .checksum, .links.self, .size, .type, .key, $rectype] | @tsv' > zipfiles.tsv
-grep software$ zipfiles.tsv > zipfiles-software.tsv
-grep dataset$ zipfiles.tsv > zipfiles-dataset.tsv
-grep -v software$ zipfiles.tsv | grep -v dataset$ > zipfiles-others.tsv
